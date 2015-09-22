7251d998e5156d6ec8229d89e8d35ba4cbbb6b81
julievdh <julievdh@mit.edu>
2015-09-22 16:46:35 -0400

    Update
    Made for all lengths at once
    Fixed method (calculate for all normal, all entangled, compare after).
    Incorporate blubber lipid content

--- Blubber/BlubberVolumeDifference.m
+++ Blubber/BlubberVolumeDifference.m
@@ -3,24 +3,37 @@
 % Calculate difference in blubber volumes and energy content between
 % entangled and non-entangled states
 
-BL = 1097; % body length
-Width = 38.63 + 0.21*BL; % body width, normal, from Fortune et al 2012
-Radius = Width/2; 
+BL = [910
+975
+1090
+1100
+1259
+1260
+1266
+1360
+1370
+1415
+1600
+1000
+1005
+1100
+1350
+1380
+1455]; % body length
 
-FullEllipsoid_NE = (4/3)*pi*(BL/2/100)*(Radius/100)*(Radius/100);
+Width = 38.63 + 0.21.*BL; % body width, normal, from Fortune et al 2012
+Radius = Width./2; 
 
-Radius_E = Width/2 - (13.8-8.3);
+FullEllipsoid_NE = (4/3)*pi.*(BL./2/100).*(Radius./100).*(Radius./100);
 
-FullEllipsoid_E = (4/3)*pi*(BL/2/100)*(Radius_E/100)*(Radius_E/100);
+Radius_E = Width./2 - (13.8-8.3);
 
-% Calculate volume difference
-Vdiff = FullEllipsoid_NE-FullEllipsoid_E; 
+FullEllipsoid_E = (4/3)*pi.*(BL./2/100).*(Radius_E./100).*(Radius_E./100);
 
-% Convert blubber volume to kJ energy
-% Blubber density 900 kg/m^3 (Parry 1949)
-% Blubber caloric content 9450 kcal/kg (REF)
-% 4.184 kJ/kcal (REF)
-Benergy = Vdiff*900*9450*4.184;
+% use Full Ellipsoid for normal whales
+% use Full Ellipsoid E for entangled whales
 
+% See MasterDataTable.xlsx sheet Table for ProcRSoc_Blubber for rest of
+% calculations 22 Sept 2015
 
 
