596c9d515735dce72835dd12af62f9b364ff7b19
julievdh <julievdh@mit.edu>
2015-11-10 16:26:32 -0500

    Changed figure orientation

--- DragTheoryComparison/daystodeath.m
+++ DragTheoryComparison/daystodeath.m
@@ -25,15 +25,14 @@ ylabel('Work (J)')
 xlim([-0.25 1.25])
 set(gca,'xticklabel',{'W_n','W_e','W_a'})
 
-subplot('position',[0.3 0.1 0.325 0.85]); hold on
+subplot('position',[0.3 0.55 0.65 0.4]); hold on
 plot(d,Wa,'b')
 % plot death threshold
 plot([0 1200],[1.86E10 1.86E10],'r--')
 plot([0 1200],[2.27E11 2.27E11],'r-')
 ylim([0 2.5E11]); xlim([0 400])
-xlabel('Days')
 
-subplot('position',[0.65 0.1 0.325 0.85]); hold on
+subplot('position',[0.3 0.1 0.65 0.4]); hold on
 plot(d,Wa,'b')
 % plot death threshold
 plot([0 4000],[1.86E10 1.86E10],'r--')
