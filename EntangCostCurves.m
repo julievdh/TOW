7e929ab76c3f7db175ae3166730ee45a74f8e9e7
julievdh <julievdh@mit.edu>
2016-03-16 12:36:34 -0400

    Added male cost to individual figures

--- EntangCostCurves.m
+++ EntangCostCurves.m
@@ -4,6 +4,9 @@
 
 % load data
 load('EntangCost') % data from 15 towed cases, Amy's 13 cases. 
+whales = {'EG 2212  ','EG 2223  ','EG 3311  ','EG 3420  ','EG 3714  ',...
+'EG 3107  ','EG 2710  ','EG 1427  ','EG 2212  ','EG 3445  ','EG 3314  ',...
+'EG 3610  ','EG 3294  ','EG 2030  ','EG 1102  '};
 warning off
 
 rightwhaleMigrate = 7.3E9/22; % van der Hoop et al. 2013 (non-entangled, one-way migration in 22 days) = cost per day
@@ -62,9 +65,11 @@ h(3).FaceColor = [247/255 129/255 121/255];
 h(4).FaceColor = [255/255 127/255 0/255];
 h(5).FaceColor = [77/255 175/255 74/255];
 
-entang = [8.667 9.4 9.38 9.4; 1.17 3.3 2.23 2.4; 5.27 9.23 7.4 7.56;...
-    8.77 20.13 8.87 19.8; 2.5 16.87 9.9 13.8; 0 24 6.27 9.53;...
-    4.7 24 1.47 3.2]; % 6 males, 1 unknown sex MEASURED
+% entang dates in MATLAB index order
+entang = [8.667,9.4,9.38,9.4;0,0,0,0;4.7,24,13.47,15.2;0,0,0,0;...
+    1.17,3.3,2.23,2.4;0,0,0,0;0,0,0,0;5.27,9.23,7.4,7.56;...
+    8.77,20.13,8.87,19.8;0,0,0,0;0,0,0,0;2.5,16.87,9.9,13.8;...
+    0,0,0,0;0,0,0,0;0,24,6.27,9.53]; % 6 males, 1 unknown sex MEASURED
 for i = 1:size(entang,1)
     jitter = rand*0.1;
 plot(entang(i,1:2)+1,[1.4+jitter 1.4+jitter],':','color',[55/255 126/255 184/255]) % +1 is becuase Dec = month 1 on plot
@@ -82,7 +87,7 @@ plot(entangARK(i,1:2)+1,[1.5+jitter 1.5+jitter],':','color',[77/255 175/255 74/2
 plot(entangARK(i,3:4)+1,[1.5+jitter 1.5+jitter],'color',[77/255 175/255 74/255])
 end
 
-xlim([1 24]); ylim([0 1.7])
+xlim([1 24]); ylim([0 2])
 set(gca,'xticklabels',{'D','J','F','M','A','M','J','J','A','S','O','N',...
     'D','J','F','M','A','M','J','J','A','S','O','N'},'xtick',1:24)
 ylabel('Relative Energetic Cost')
@@ -108,3 +113,49 @@ plot([13 13],[0 2.5],'k:')
 plot([25 25],[0 2.5],'k:')
 xlabel('Time'); ylabel('Relative Energetic Cost')
 adjustfigurefont
+
+
+%% ADD ENTANGLEMENT TO MALE BUDGET
+for i = 1:length(entang); % let's use case 3 as an example 
+
+% get relative costs of things
+rel1 = Wa(i)/rightwhaleMigrate;
+rel2 = Wa(i)/rightwhaleFor;
+
+% add to data_male
+% add maximum duration
+% get indices
+mx_ind = floor(entang(i,1)):ceil(entang(i,2));
+if mx_ind > 0
+data_male(mx_ind,6) = rel1*data_male(mx_ind,2);
+end
+mn_ind = floor(entang(i,3)):ceil(entang(i,4));
+if mn_ind > 0
+data_male(mn_ind,7) = rel1*data_male(mn_ind,2);
+% make indices for minimum = 0 for maximum duration (so don't pile on top
+% of each other)
+data_male(mn_ind,6) = 0;
+end
+
+
+figure(i); clf; hold on
+h = area(data_male);
+h(1).FaceColor = [152/255 78/255 163/255];
+h(2).FaceColor = [228/255 26/255 28/255];
+h(3).FaceColor = [247/255 129/255 121/255];
+h(4).FaceColor = [255/255 127/255 0/255];
+h(5).FaceColor = [77/255 175/255 74/255];
+h(6).FaceColor = 'w';
+h(7).FaceColor = [55/255 126/255 184/255];
+
+xlim([1 24]);
+set(gca,'xticklabels',{'D','J','F','M','A','M','J','J','A','S','O','N',...
+    'D','J','F','M','A','M','J','J','A','S','O','N'},'xtick',1:24)
+ylabel('Relative Energetic Cost')
+title(whales(i))
+adjustfigurefont
+
+% clear data male for next whale
+data_male(:,6:7) = 0;
+pause
+end
