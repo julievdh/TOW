e79d5ef60a6daf28f60d7465ff920bfb757aae31
julievdh <julievdh@mit.edu>
2016-03-16 11:44:41 -0400

    Fixed costs/day

--- EntangCostCurves.m
+++ EntangCostCurves.m
@@ -6,8 +6,8 @@
 load('EntangCost') % data from 15 towed cases, Amy's 13 cases. 
 warning off
 
-rightwhaleMigrate = 7.3E9; % van der Hoop et al. 2013 (non-entangled, one-way migration in 22 days)
-rightwhaleRepro = 7.9E8; % Klanjscek et al 2007 
+rightwhaleMigrate = 7.3E9/22; % van der Hoop et al. 2013 (non-entangled, one-way migration in 22 days) = cost per day
+rightwhaleRepro = 5.8E11/(365*2); % Klanjscek et al 2007: 5.8E11 J over 2 years
 rightwhalePreg = (2090-1906)*10^6; % Fortune et al 2013 (1906 = resting female costs/day)
 rightwhaleLac = (4120-1906)*10^6; % Fortune et al 2013 (1906 = resting female costs/day)'
 rightwhaleFor = 500E6; % Foraging cost per day McGregor et al 2013
@@ -41,8 +41,19 @@ plot(t(1:365),rightwhalePreg*t(1:365),'color',[152/255 152/255 152/255],'LineWid
 plot(t(1:365),rightwhaleLac*t(1:365),'color',[152/255 152/255 152/255],'LineWidth',1.5);
 plot(t(1:305),rightwhaleFor*t(1:305),'color',[152/255 152/255 152/255],'LineWidth',1.5);
 
+% these relative costs should form the basis of the timelines below
+figure(17); clf; hold on
+plot(0,rightwhaleMigrate,'o')
+plot(0,rightwhaleRepro,'o')
+plot(0,rightwhalePreg,'o')
+plot(0,rightwhaleLac,'o')
+plot(0,rightwhaleFor,'o')
+legend('Migration','Reproduction','Pregnancy','Lactation','Foraging')
+
+
 %% 
 % plot male budget
+figure(109)
 subplot('position',[0.4 0.55 0.58 0.4]); hold on; box on
 h = area(data_male);
 h(1).FaceColor = [152/255 78/255 163/255];
@@ -54,16 +65,26 @@ h(5).FaceColor = [77/255 175/255 74/255];
 entang = [8.667 9.4 9.38 9.4; 1.17 3.3 2.23 2.4; 5.27 9.23 7.4 7.56;...
     8.77 20.13 8.87 19.8; 2.5 16.87 9.9 13.8; 0 24 6.27 9.53;...
     4.7 24 1.47 3.2]; % 6 males, 1 unknown sex MEASURED
-% ADD ESTIMATED HERE
 for i = 1:size(entang,1)
     jitter = rand*0.1;
-plot(entang(i,1:2),[1.4+jitter 1.4+jitter],':','color',[55/255 126/255 184/255])
-plot(entang(i,3:4),[1.4+jitter 1.4+jitter],'color',[55/255 126/255 184/255])
+plot(entang(i,1:2)+1,[1.4+jitter 1.4+jitter],':','color',[55/255 126/255 184/255]) % +1 is becuase Dec = month 1 on plot
+plot(entang(i,3:4)+1,[1.4+jitter 1.4+jitter],'color',[55/255 126/255 184/255])
 end
 
-xlim([1 24]); ylim([0 1.6])
+% Estimated ARK cases:
+entangARK = [6.83 10.83 10.83 10.83; 5.27 21.23 7.4 7.87; ...
+    8.67 20.03 18.067 18.8; 8.87 9.4 9.4 9.4; 3.57 10.53 7.67 7.67;...
+    1.8 5.43 5.43 5.43; 0.767 24 4.23 18.8; 8.3 11.56 11.56 11.56;...
+    7.33 9.87 9.87 9.87]; % 7 males, 2 unknown sex ESTIMATED [1 unknown sex (3392 has insuff sightings info)
+for i = 1:size(entangARK,1)
+    jitter = rand*0.1;
+plot(entangARK(i,1:2)+1,[1.5+jitter 1.5+jitter],':','color',[77/255 175/255 74/255])
+plot(entangARK(i,3:4)+1,[1.5+jitter 1.5+jitter],'color',[77/255 175/255 74/255])
+end
+
+xlim([1 24]); ylim([0 1.7])
 set(gca,'xticklabels',{'D','J','F','M','A','M','J','J','A','S','O','N',...
-    'D','J','F','M','A','M','J','J','A','S','O','N'})
+    'D','J','F','M','A','M','J','J','A','S','O','N'},'xtick',1:24)
 ylabel('Relative Energetic Cost')
 
 % plot female budget
@@ -85,5 +106,5 @@ xlim([1 36]); set(gca,'xticklabels',{'D','J','F','M','A','M','J','J',...
     'D','J','F','M','A','M','J','J','A','S','O','N'},'xtick',1:36)
 plot([13 13],[0 2.5],'k:')
 plot([25 25],[0 2.5],'k:')
-xlabel('Time'); 
+xlabel('Time'); ylabel('Relative Energetic Cost')
 adjustfigurefont
