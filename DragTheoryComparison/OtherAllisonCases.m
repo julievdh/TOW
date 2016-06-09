580bdb2d14086c21183b9ba7c2aec6b657f5ea65
julievdh <julievdh@mit.edu>
2016-06-09 06:51:25 -0400

    Assessment of cases from AGH

---
+++ DragTheoryComparison/OtherAllisonCases.m
@@ -0,0 +1,15 @@
+% OtherAllisonCases
+% Assess drag/SI timelines for other cases from Allison Henry
+% Drag events for Julie.xlsx
+
+%% 1. Eg 3111
+CriticalEstimate(10,[],36,0,[],[])
+CriticalEstimate(10,[],6,0,[],[])
+
+%% 2. Eg 3445
+
+
+%% 3. Eg 1019 Radiator
+flt = [1 0.4086 0.5];
+CriticalEstimate(29,[],30.5,flt,[],[])
+CriticalEstimate(29,[],21.3,flt,[],[])
